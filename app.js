ceb8eeb979c3a15a942a7f8306cb64b228bb4406
Verginie <hi_cherifi@esi.dz>
2019-05-25 06:32:05 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const YOUTUBE_END_POINT = "https://www.googleapis.com/youtube/v3/search";
+
+function getDataFromApi(searchTerm , callback){
+    const settings ={
+        url : YOUTUBE_END_POINT,
+        data : {
+            part : 'snippet',
+            key : 'AIzaSyDp6GqDnV0luhPdipKWBS7oSVCaDb87kBQ',
+            q : searchTerm
+        },
+        dataType: 'json',
+        type: 'GET',
+        success: callback
+    };
+    $.ajax(settings);
+}
+function renderVideo(result){
+    let ID = result.id.videoId ;
+    let thumbnail = result.snippet.thumbnails.medium.url;
+    let title =  result.snippet.title;
+    let channel = result.snippet.channelTitle;
+    return `<div class="item">
+    <a href="https://www.youtube.com/watch?v=${ID}"><img src=${thumbnail} alt=""></a>
+    <h4>${title}</h4>
+    <span>${channel}</span>
+    </div>`;
+}
+function displayData(data){
+    let videos = data.items.map((item,index)=>renderVideo(item));
+    $('.js-display-result').html(videos);
+}
+$(function(){
+    $('.js-form').submit(event =>{
+    event.preventDefault();
+    let searchTerm = $(this).find('#search-tab').val();
+    getDataFromApi(searchTerm,displayData);
+
+});
+});
